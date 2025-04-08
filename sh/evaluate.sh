35e0038c4075ae910f7a604a278f3375989c26b1
Nihal Jain <nihal.b.jain@gmail.com>
2025-04-08 10:22:27 -0400

    Update evaluation run script

--- sh/evaluate.sh
+++ sh/evaluate.sh
@@ -1,5 +1,10 @@
 python3 evaluate.py \
-    --max_samples -1  \
-    --temperature 0.0 \
-    --n_samples 1 \
-    --batch_size 10
+    --max_samples -1 \
+    --temperature 1.0 \
+    --n_samples 5 \
+    --batch_size 50 \
+    --response_extractor qwen \
+    --seed 100 \
+    --max_new_tokens 1500 \
+    --use_compile \
+    --load_in_half
